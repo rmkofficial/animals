0152d96f5e18e8602d9e43d759bf341cb39e2c05
Ramazan Mert Kaya <kmert0@outlook.com>
2023-04-24 20:39:09 +0300

    State System

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import { useState } from "react";
+
 function App() {
-  // const handleClick = () => {
-  //   console.log(`Button was clicked`);
-  // };
+  const [count,setCount] = useState(0);
+
+  const handleClick = () => {
+    setCount(count + 1);
+  };
   return (
     <div>
-      <button onClick={() => console.log(`Button was clicked!`)}>Add Animal</button>
+      <button onClick={handleClick}>Add Animal</button>
+      <div>
+        Number of animals: {count}
+      </div>
     </div>
   );
 }
